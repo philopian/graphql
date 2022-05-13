8d2c74ffd71031e26a27fb55a58f2248c4255c90
Phil Willis <philopian@gmail.com>
2022-05-12 22:31:03 -0700

    allow for REST and GraphQL

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,13 @@ import resolvers from "./schemas/resolvers";
 const PORT = 3000;
 const app = express();
 
+// You can also do all REST API stuff with express!
+app.get("/", (req: Request, res: Response) => {
+  res.send({ hello: "rest example too!!" });
+});
+
 const graphqlServer = new ApolloServer({ typeDefs, resolvers });
 graphqlServer.start().then((res) => {
   graphqlServer.applyMiddleware({ app });
-  app.listen({ port: 3000 }, () => console.log(`Now browse to http://localhost:${PORT}${graphqlServer.graphqlPath}`));
+  app.listen({ port: 3000 }, () => console.log(`🚀  Server ready at http://localhost:${PORT}${graphqlServer.graphqlPath}`));
 });
